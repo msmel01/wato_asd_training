7525969f7b93ed1445da3fd19f23f3a9184a07c8
wk5ng <wk5ng@uwaterloo.ca>
2025-05-18 13:45:59 -0400

    Working control node

--- src/robot/control/src/control_node.cpp
+++ src/robot/control/src/control_node.cpp
@@ -33,6 +33,10 @@ void PurePursuitController::controlLoop() {
   const auto &goal = current_path_->poses.back().pose.position;
   const auto &robot_pos = robot_odom_->pose.pose.position;
   if (computeDistance(robot_pos, goal) <= goal_tolerance_) {
+    geometry_msgs::msg::Twist cmd_vel;
+    cmd_vel.linear.x = 0.0;
+    cmd_vel.angular.z = 0.0;
+    cmd_vel_pub_->publish(cmd_vel);
     return;
   }
 
@@ -40,7 +44,11 @@ void PurePursuitController::controlLoop() {
   // Find the lookahead point
   auto lookahead_point = findLookaheadPoint();
   if (!lookahead_point) {
-      return;  // No valid lookahead point found
+    geometry_msgs::msg::Twist cmd_vel;
+    cmd_vel.linear.x = 0.0;
+    cmd_vel.angular.z = 0.0;
+    cmd_vel_pub_->publish(cmd_vel);
+    return ;  // No valid lookahead point found
   }
 
   // Compute velocity command
@@ -62,18 +70,25 @@ std::optional<geometry_msgs::msg::PoseStamped> PurePursuitController::findLookah
 
 geometry_msgs::msg::Twist PurePursuitController::computeVelocity(const geometry_msgs::msg::PoseStamped &target) {
   geometry_msgs::msg::Twist cmd_vel;
-  // Robot's linear speed is kept constant
-  cmd_vel.linear.x = linear_speed_;
-  cmd_vel.linear.y = linear_speed_;
-  cmd_vel.linear.z = 0;
 
-  // Adjust angular velocity based on curvature
-  const tf2::Quaternion inverse_robot_q(
-    robot_odom_->pose.pose.orientation.x,
-    robot_odom_->pose.pose.orientation.y,
-    robot_odom_->pose.pose.orientation.z,
-    -1 * robot_odom_->pose.pose.orientation.w
-  );
+  RCLCPP_INFO(this->get_logger(), "Current robot point at %f, %f", robot_odom_->pose.pose.position.x, robot_odom_->pose.pose.position.y);
+  RCLCPP_INFO(this->get_logger(), "Lookahead point at %f, %f", target.pose.position.x, target.pose.position.y);
+
+  const auto &goal = current_path_->poses.back().pose.position;
+  const auto &robot_pos = robot_odom_->pose.pose.position;
+  if (computeDistance(robot_pos, goal) <= goal_tolerance_) {
+    geometry_msgs::msg::Twist cmd_vel;
+    cmd_vel.linear.x = 0.0;
+    cmd_vel.angular.z = 0.0;
+    return cmd_vel;
+  }
+  
+  const auto& robot_position = robot_odom_->pose.pose.position;
+  const auto& robot_orientation = robot_odom_->pose.pose.orientation;
+  double current_yaw = extractYaw(robot_orientation);
+
+  const auto& target_position = target.pose.position;
+  const auto& target_orientation = target.pose.orientation;
 
   const tf2::Quaternion target_q(
     target.pose.orientation.x,
@@ -82,15 +97,24 @@ geometry_msgs::msg::Twist PurePursuitController::computeVelocity(const geometry_
     target.pose.orientation.w
   );
 
-  tf2::Quaternion relative_q = target_q * inverse_robot_q;
+  const geometry_msgs::msg::Quaternion target_quat = tf2::toMsg(target_q);
+  //tf2::Quaternion relative_q = target_q * inverse_robot_q;
 
-  const geometry_msgs::msg::Quaternion relative_q_quat = tf2::toMsg(relative_q);
+  double angle_to_target = std::atan2(target_position.y - robot_position.y, target_position.x - robot_position.x);
 
-  double steering_angle = extractYaw(relative_q_quat);
+  //const geometry_msgs::msg::Quaternion relative_q_quat = tf2::toMsg(relative_q);
 
-  cmd_vel.angular.x = std::cos(steering_angle) * linear_speed_;
-  cmd_vel.angular.y = std::sin(steering_angle) * linear_speed_;
-  cmd_vel.angular.z = 0;
+  double steering_angle = angle_to_target - current_yaw;
+
+  //cmd_vel.angular.x = std::cos(steering_angle) * linear_speed_;
+  //cmd_vel.angular.y = std::sin(steering_angle) * linear_speed_;
+
+  while (steering_angle > M_PI) steering_angle -= 2 * M_PI;
+  while (steering_angle < -M_PI) steering_angle += 2 * M_PI;
+
+  // Robot's linear speed is kept constant
+  cmd_vel.linear.x = linear_speed_;
+  cmd_vel.angular.z = 1.4 * steering_angle;
 
   return cmd_vel;
 }
