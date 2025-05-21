5109159996d587ced88dcda2badb06d8ad639825
wk5ng <wk5ng@uwaterloo.ca>
2025-05-20 21:32:14 -0400

    This is supposed to fix the jittering (and it did) but then it didn't

--- src/robot/costmap/include/costmap_node.hpp
+++ src/robot/costmap/include/costmap_node.hpp
@@ -6,24 +6,52 @@
 
 #include "sensor_msgs/msg/laser_scan.hpp"
 #include "nav_msgs/msg/occupancy_grid.hpp"
-
+ 
 #include "costmap_core.hpp"
- 
+
 class CostmapNode : public rclcpp::Node {
-  public:
-    CostmapNode();
-    
-    void publishGrid();
-
-    void subscribeMessage(sensor_msgs::msg::LaserScan::SharedPtr scan);
-
-  private:
+    public:
+      CostmapNode();
+  
+      void laserCallback(const sensor_msgs::msg::LaserScan::SharedPtr scan);
+      
+      void publishCostmap();
+      
+      void initializeCostmap();
+      
+      void convertToGrid(float range, float angle, int &x_grid, int &y_grid);
+      
+      void markObstacle(int x_grid, int y_grid);
+      
+      void inflateObstacles();
+      
+      nav_msgs::msg::OccupancyGrid::SharedPtr grid_msg_;
+  
+    private:
     robot::CostmapCore costmap_;
-    
-    rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr occup_grid_pub_;
+  
+      rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr costmap_pub_;
+      rclcpp::Publisher<std_msgs::msg::String>::SharedPtr string_pub_;
+      rclcpp::TimerBase::SharedPtr timer_;
+  
+      rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_sub_;
 
-    // Subscribe to read
-    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr scan_subs_;
-};
- 
-#endif 
+      std::vector<std::vector<int>> costmap;
+
+      static constexpr double resolution_ = 0.1;
+      // Value set to cells with an obstacle
+      static constexpr int obstacle_cost_ = 100;
+      // width and height
+      int width_; 
+      int height_;  
+      // origin coordinates
+      int origin_x_;
+      int origin_y_;
+
+      // Inflation radius and costs
+      static constexpr float inflation_radius_ = 1.0f;
+      static constexpr int inflation_radius_cells_ = static_cast<int>(inflation_radius_ / resolution_);
+      static constexpr int max_inflation_cost_ = 100; // make the cost must higher to give higher weight to obstacles
+  };
+   
+  #endif 
--- src/robot/costmap/src/costmap_node.cpp
+++ src/robot/costmap/src/costmap_node.cpp
@@ -1,80 +1,133 @@
 #include <chrono>
 #include <memory>
-#include <functional>
-
-using std::placeholders::_1;
-
+ 
 #include "costmap_node.hpp"
  
-
 CostmapNode::CostmapNode() : Node("costmap"), costmap_(robot::CostmapCore(this->get_logger())) {
-  occup_grid_pub_ = this->create_publisher<nav_msgs::msg::OccupancyGrid>("/costmap", 10);
-  scan_subs_ = this->create_subscription<sensor_msgs::msg::LaserScan>("/lidar", 10, std::bind(&CostmapNode::subscribeMessage, this, _1));
-}
-
-
-void CostmapNode::publishGrid() {
-  nav_msgs::msg::OccupancyGrid grid_msg;
-
-  grid_msg.header.stamp = now();
-  // grid_msg.header.frame_id = "base_link"; // frame is robot-centered
-  // grid_msg.header.frame_id = "robot/chassis/lidar"; // frame is robot-centered
-  grid_msg.header.frame_id = "robot/chassis/lidar"; // frame is robot-centered
-
-  grid_msg.info.resolution = costmap_.getResolution();
-  grid_msg.info.width = costmap_.getWidth(); // width of grid array
-  grid_msg.info.height = costmap_.getHeight(); // height of grid array
-  grid_msg.info.origin.position.x = -costmap_.getWidthWorld() / 2.0;
-  grid_msg.info.origin.position.y = -costmap_.getHeightWorld() / 2.0;
-  grid_msg.info.origin.position.z = 0.0;
-  grid_msg.info.origin.orientation.w = 1.0;
-
-  grid_msg.data.resize(grid_msg.info.width * grid_msg.info.height);
-
-  for (size_t y = 0; y < grid_msg.info.height; ++y) {
-    for (size_t x = 0; x < grid_msg.info.width; ++x) {
-      int value = costmap_.getCost(x, y);
-      int8_t mapped_value = (value >= 100) ? 100 : static_cast<int8_t>(value); // clamp to max 100 and data type required for occupancy grid
-      grid_msg.data[y * grid_msg.info.width + x] = mapped_value;
-    }
-  }
-
-  // int max_val = *std::max_element(grid_msg.data.begin(), grid_msg.data.end());
-  // RCLCPP_INFO(this->get_logger(), "Max cost in grid: %d", max_val); // check that there are non-zero values
-
-  occup_grid_pub_->publish(grid_msg);
-}
-
-
-void CostmapNode::subscribeMessage(sensor_msgs::msg::LaserScan::SharedPtr scan) {
-  // RCLCPP_INFO(this->get_logger(), "LaserScan frame_id: %s", scan->header.frame_id.c_str());
-
-  costmap_.initializeGrid();
-  // RCLCPP_INFO(this->get_logger(), "Finished initializeGrid");
-  for (size_t i = 0; i < scan->ranges.size(); ++i) {
-    double angle = scan->angle_min + i * scan->angle_increment;
-    double range = scan->ranges[i];
-    if (range < scan->range_max && range > scan->range_min) {
-        // Calculate grid coordinates
-        int x_grid, y_grid;
-        // RCLCPP_INFO(this->get_logger(), "Started convertToGrid");
-        costmap_.convertToGrid(range, angle, x_grid, y_grid);
-        // RCLCPP_INFO(this->get_logger(), "Finished convertToGrid");
-        // RCLCPP_INFO(this->get_logger(), "Started markObstacle");
-        costmap_.markObstacle(x_grid, y_grid);
-        // RCLCPP_INFO(this->get_logger(), "Ended markObstacle");
-    }
-}
-
-  costmap_.inflateObstacles();
-
-  this->publishGrid();
-  // RCLCPP_INFO(this->get_logger(), "New local costmap published");
+  // Initialize the constructs and their parameters
+  laser_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>("/lidar", 10, std::bind(&CostmapNode::laserCallback, this, std::placeholders::_1));
   
-  // RCLCPP_INFO(this->get_logger(), "Finished subscribeMessage");
+  costmap_pub_ = this->create_publisher<nav_msgs::msg::OccupancyGrid>("/costmap", 10);
+  //timer_ = this->create_wall_timer(std::chrono::milliseconds(100), std::bind(&CostmapNode::publishCostmap, this));
+
+  width_ = 60 / resolution_;
+  height_ = 60 / resolution_;
+  origin_x_ = -30;
+  origin_y_ = -30;
+  
+  grid_msg_ = std::make_shared<nav_msgs::msg::OccupancyGrid>();
+  grid_msg_->info.resolution = resolution_;
+  grid_msg_->info.width = width_;
+  grid_msg_->info.height = height_;
+  grid_msg_->info.origin.position.x = origin_x_;
+  grid_msg_->info.origin.position.y = origin_y_;
+  grid_msg_->info.origin.position.z = 0.0;
+  grid_msg_->info.origin.orientation.w = 1.0;
+}
+
+void CostmapNode::laserCallback(const sensor_msgs::msg::LaserScan::SharedPtr scan) {
+    // Step 1: Initialize costmap
+    initializeCostmap();
+    grid_msg_->header.stamp = now();
+    grid_msg_->header.frame_id = scan->header.frame_id;
+
+ 
+    // Step 2: Convert LaserScan to grid and mark obstacles
+    for (size_t i = 0; i < scan->ranges.size(); ++i) {
+        double angle = scan->angle_min + i * scan->angle_increment;
+        double range = scan->ranges[i];
+        if (range < scan->range_max && range > scan->range_min) {
+            // Calculate grid coordinates
+            int x_grid, y_grid;
+            convertToGrid(range, angle, x_grid, y_grid);
+            markObstacle(x_grid, y_grid);
+        }
+    }
+ 
+    // Step 3: Inflate obstacles
+    inflateObstacles();
+ 
+    // Step 4: Publish costmap
+    publishCostmap();
+}
+
+void CostmapNode::initializeCostmap() {
+    // RCLCPP_INFO(logger_, "Initializing costmap with width: %d and height: %d", width_costmap_arr_, height_costmap_arr_);
+    costmap = std::vector<std::vector<int>>(height_, std::vector<int>(width_, 0));
+    grid_msg_->data.clear();
+    grid_msg_->data.resize(
+        grid_msg_->info.width * grid_msg_->info.height, 0
+    );
+}
+
+void CostmapNode::convertToGrid(float range, float angle, int &x_grid, int &y_grid){
+    // map's resolution
+    x_grid = (range * cos(angle) - origin_x_) / resolution_;
+    y_grid = (range * sin(angle) - origin_y_) / resolution_;
 }
 
 
+void CostmapNode::markObstacle(int x_grid, int y_grid) {
+    // this->costmap[y_grid][x_grid] = this->obstacle_cost_;
+
+    if (x_grid > 0 && x_grid < width_ && y_grid > 0 && y_grid < height_) {
+        costmap[static_cast<int>(y_grid)][static_cast<int>(x_grid)] = obstacle_cost_;
+    }
+}
+
+void CostmapNode::inflateObstacles() {
+    for (int y = 0; y < height_; ++y) {
+        for (int x = 0; x < width_; ++x) {
+            if (costmap[y][x] == obstacle_cost_) {
+                // This is an obstacle cell
+
+                for (int dy = -inflation_radius_cells_; dy <= inflation_radius_cells_; ++dy) {
+                    for (int dx = -inflation_radius_cells_; dx <= inflation_radius_cells_; ++dx) {
+                        int x_neighbor = x + dx; // index of neighbor in costmap array
+                        int y_neighbor = y + dy;
+                        
+                        if (x_neighbor < 0 || x_neighbor >= width_ || y_neighbor < 0 || y_neighbor >= height_) {
+                            // Skip if index out of bounds of array
+                            continue;
+                        }
+                        
+                        // (x - x_neighbor)^2 + (y - y_neighbor)^2 = (x - (x + dx))^2 + (y - (y + dy))^2 
+                        float euclidean = std::sqrt(dx*dx+ dy*dy) * resolution_;
+
+                        if (euclidean > inflation_radius_ || euclidean == 0.0f) {
+                            continue; // Make sure within radius
+                        }
+
+                        int inflated_cost = static_cast<int>(max_inflation_cost_ * (1.0f - euclidean / inflation_radius_));
+
+                        // // NEW
+                        // size_t x_i = static_cast<size_t>(x_neighbor);
+                        // size_t y_i = static_cast<size_t>(y_neighbor);
+
+                        if (inflated_cost > costmap[y_neighbor][x_neighbor]) {
+                            costmap[y_neighbor][x_neighbor] = inflated_cost;
+                        }
+                    }
+                }
+
+                // RCLCPP_INFO(logger_, "Obstacle at grid (%d, %d)", x, y);
+            }
+        }
+    }
+}
+ 
+void CostmapNode::publishCostmap() {
+    for (size_t y = 0; y < grid_msg_->info.height; ++y) {
+        for (size_t x = 0; x < grid_msg_->info.width; ++x) {
+          int value = costmap[y][x];
+          int8_t mapped_value = (value >= 100) ? 100 : value; // clamp to max 100 and data type required for occupancy grid
+          grid_msg_->data[y * grid_msg_->info.width + x] = mapped_value;
+        }
+      }
+    
+    costmap_pub_->publish(*(grid_msg_));
+}
+ 
 int main(int argc, char ** argv)
 {
   rclcpp::init(argc, argv);
